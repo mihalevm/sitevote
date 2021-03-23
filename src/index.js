01219e6f69d2d566f83939218209ce838cd6cbf7
Egor Nagornov <nagornov_e@bk.ru>
2021-03-23 21:11:00 +0700

    Шаблон для index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // import 'bootstrap';
 import './styles/style.scss';
-$('#link-general').click(function(){
-  $('#link-vote').hide()
+$('#auth-enter').click(function(){
+  $('#auth-modal').show();
+  $('#auth-modal-close-1').click(function() {
+    $('#auth-modal').hide();
+  });
+  $('#auth-modal-close-2').click(function() {
+    $('#auth-modal').hide();
+  });
 });
--- src/js/profile.js
+++ src/js/profile.js
@@ -1,1 +1,10 @@
 import '../styles/style.scss';
+$('#auth-enter').click(function(){
+  $('#auth-modal').show();
+  $('#auth-modal-close-1').click(function() {
+    $('#auth-modal').hide();
+  });
+  $('#auth-modal-close-2').click(function() {
+    $('#auth-modal').hide();
+  });
+});
