e1d79967312c8825df5bb31a8da2fb50a75a0d17
Egor Nagornov <nagornov_e@bk.ru>
2021-03-27 13:59:25 +0700

    Перенос html в JS шаблоны

--- src/js/profile.js
+++ src/js/profile.js
@@ -1,3 +1,44 @@
-import { windowAuth } from './templates/main.templ';
+import { windowAuth, header } from './templates/main.templ';
 import '../styles/style.scss';
-windowAuth(document.body)
+const container = () => `
+  <div class="container">
+    <div class="row">
+      <main>
+        <div class="container pt-5">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="#">Профиль</a></li>
+            </ol>
+          </nav>
+        </div>
+        <div class="container pt-5">
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">ФИО</span>
+            <input type="text" class="form-control" placeholder="Иванов Иван Иванович" aria-label="Username" aria-describedby="basic-addon1">
+          </div>          
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon2">Email</span>
+            <input type="text" class="form-control" placeholder="index@google.com" aria-label="Recipient's username" aria-describedby="basic-addon2">
+          </div>          
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon3">Номер телефона</span>
+            <input type="text" class="form-control" placeholder="+7(999)999-77-88" aria-label="Recipient's username" aria-describedby="basic-addon2">
+          </div>   
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon4">Имя пользователя</span>
+            <input type="text" class="form-control" placeholder="ivanovivan" aria-label="Recipient's username" aria-describedby="basic-addon2">
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="basic-addon5">Пароль</span>
+            <input type="password" class="form-control" id="user-password">
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+`;
+
+header(document.body).done(function() {
+  $(document.body).append(container);
+})
+windowAuth(document.body);
--- src/js/templates/main.templ.js
+++ src/js/templates/main.templ.js
@@ -48,3 +48,31 @@ export const windowAuth = (el) => {
     });
   });  
 };
+
+export const header = (el) => {
+  const tmpl = ({title, enter}) => `
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">${title}</a>
+      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a id="auth-enter" class="nav-link" href="#">${enter}</a>
+        </li>
+      </ul>
+    </header>
+  `;
+
+  return $.ajax({
+    type: 'GET',
+    url: '/config/config.json',
+  }).done(function(data){
+    const content = data.auth;
+    $(el).append(tmpl({ 
+      title: content.title,
+      enter: content.enter
+    }));
+  });
+}
