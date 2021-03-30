69bc3f3bab6d445c4f31724c6d55b341d880d86a
Egor Nagornov <nagornov_e@bk.ru>
2021-03-30 12:26:43 +0700

    Изменение шаблона карточек

--- src/js/templates/card.tmpl.js
+++ src/js/templates/card.tmpl.js
@@ -7,13 +7,29 @@ const tmpl = (header) => `
 <div class="card" style="width: 18rem;">
   <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
   <div class="card-body">
-    <h6>${header}</h6>
+    <h6 class="ch">${header}</h6>
   </div>
 </div>
 </div>
 `;
 
-  $.each(config.cards_tmpl, function(i, v){    
-    $(el).append(tmpl(v));
-  });  
+let counter = 1;
+let cards = '';
+const beginTag = '<div class="row row-cols-1 row-cols-md-4 g-4 pt-3">\n';
+const endTag = '\n</div>\n';
+$.each(config.cards_tmpl, function(i, v){
+  if(counter == 1) {      
+    cards = cards + beginTag + tmpl(v);        
+  }
+  if(counter >= 2 && counter < 4) {
+    cards = cards + tmpl(v);
+  }        
+  if(counter == 4) {
+    cards = cards + tmpl(v) + endTag;    
+    counter = 1;    
+  } else {
+    counter++;     
+  }  
+}); 
+$(el).append(cards);
 }
