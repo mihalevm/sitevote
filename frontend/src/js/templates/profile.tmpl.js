475fd6d946a37f2c4f69f3d82d2b43ff79b843f2
Egor Nagornov <nagornov_e@bk.ru>
2021-04-06 16:18:06 +0700

    Переделка раздела добавление сайта и статистики, добавление тестовых картинок

--- frontend/src/js/templates/profile.tmpl.js
+++ frontend/src/js/templates/profile.tmpl.js
@@ -53,22 +53,14 @@ Chart.register(
 );
 
 export const createProfile = (el) => {
-const tmpl = ({ fio, email, number, username, company_name, confirm_pass, pass, save }) => `
+const tmpl = ({ fio_company, email, number, confirm_pass, pass, save }) => `
 <div class="container">
 <div class="row pt-5">
   <main>
     <form id="form-1" action="#" class="needs-validation" novalidate>
-    <div class="mb-3">          
-      <div class="form-check mb-3">
-        <input class="form-check-input" type="radio" name="profile-select-fio-company" id="profile-fio-check" >
-        <label for="profile-fio" class="form-label">${fio}</label>
-        <input id="profile-fio" type="text" class="form-control" placeholder="Иванов Иван Иванович" aria-label="fio" aria-describedby="profile-fio" disabled>
-      </div>
-      <div class="form-check mb-3">
-        <input class="form-check-input" type="radio" name="profile-select-fio-company" id="profile-company-check" checked>
-        <label for="profile-company" class="form-label">${company_name}</label>
-        <input id="profile-company" type="text" class="form-control"  placeholder="ООО 'Компания'" aria-label="fio" aria-describedby="profile-fio">
-      </div>
+    <div class="mb-3">      
+      <label for="profile-fio" class="form-label">${fio_company}</label>
+      <input id="profile-fio" type="text" class="form-control" placeholder="Иванов Иван Иванович\\ООО 'Компания'" aria-label="fio" aria-describedby="profile-fio">      
     </div>          
     <div class="mb-3">
       <label for="profile-email" class="form-label">${email}</label>
@@ -77,10 +69,6 @@ const tmpl = ({ fio, email, number, username, company_name, confirm_pass, pass, 
     <div class="mb-3">
       <label for="profile-number" class="form-label">${number}</label>
       <input id="profile-number" type="text" class="form-control" placeholder="+7(999)999-77-88" aria-label="number" aria-describedby="profile-number">
-    </div>   
-    <div class="mb-3">
-      <label for="profile-username" class="form-label">${username}</label>
-      <input id="profile-username" type="text" class="form-control" placeholder="ivanovivan" aria-label="username" aria-describedby="profile-username">
     </div>
     <div class="mb-3">
       <label for="profile-password" class="form-label">${pass}</label>
@@ -98,11 +86,9 @@ const tmpl = ({ fio, email, number, username, company_name, confirm_pass, pass, 
 `
 
 $(el).append(tmpl({ 
-  fio: config.profile.fio,
-  company_name: config.profile.company_name,
+  fio_company: config.profile.fio_company,  
   email: config.profile.email,
-  number: config.profile.number,
-  username: config.profile.username,
+  number: config.profile.number,  
   confirm_pass: config.profile.confirm_pass,
   pass: config.profile.pass,
   save: config.profile.save,
@@ -125,62 +111,29 @@ const checkEqualPasswords = (pass, passConfirm) => {
   }
 };
 
-// Init state can be diff
-const changeFieldEnable = () => {
-  $('#profile-fio-check').on("click", function() {
-    $('#profile-company').attr('disabled', 'true');
-    $('#profile-fio').removeAttr('disabled');
-  });
-  
-  $('#profile-company-check').on("click", function() {
-    $('#profile-fio').attr('disabled', 'true');
-    $('#profile-company').removeAttr('disabled');
-  });
-}
-
-changeFieldEnable();
-
 $('#profile-save').on('click', function() {
   checkEqualPasswords($('#profile-password').val(), $('#profile-confirm-pass').val());
 });
 };
 
-export const createAddSite = (el) => {
-  const tmpl = ({ save, url, access_check, pic, description, uniq_url }) => `
-  <div class="container">
-  <div class="row">
-    <main>
-      <div class="container pt-5">
-        <div class="mb-3">
-          <label for="profile-url" class="form-label">${url}</label>
-          <input type="text" class="form-control" placeholder="https://google.com" aria-label="profile-url" aria-describedby="profile-url">
-        </div>
-        <div class="mb-3">
-          <button id="profile-access-check" class="btn btn-primary">${access_check}</button>
-          <label for="profile-access-check" class="form-label">${pic}</label>
-        </div>
-        <div class="mb-3">          
-          <label for="profile-site-description" class="form-label">${description}</label>          
-          <textarea id="profile-site-description" class="form-control" placeholder="Описание сайта" style="height: 100px"></textarea>
-        </div>
-        <div class="mb-3">
-        <label for="profile-uniq-url" class="form-label">${uniq_url}</label>
-          <input type="text" class="form-control" placeholder="https://link.li/" aria-label="profile-uniq-url" aria-describedby="profile-uniq-url">
-        </div>
-        <button id="profile-site-save" type="submit" class="btn btn-primary">${save}</button>
-      </div>
-    </main>
+export const createProfileTabs = (el) => {
+  const tmpl = ({profile, statistics}) => `
+  <nav>
+  <div class="nav nav-tabs" id="profile-nav-btns" role="tablist">
+    <button class="nav-link active" id="profile-btn-tab" data-bs-toggle="tab" data-bs-target="#profile-tab" type="button" role="tab" aria-controls="" aria-selected="true">${profile}</button>  
+    <button class="nav-link" id="statistics-btn-tab" data-bs-toggle="tab" data-bs-target="#statistics-tab" type="button" role="tab" aria-controls="" aria-selected="false">${statistics}</button>
+  </nav>
+  <div class="tab-content" id="profile-tabs">
+    <div class="tab-pane fade show active p-3" id="profile-tab" role="tabpanel" aria-labelledby="">
+    </div>  
+    <div class="tab-pane fade p-3" id="statistics-tab" role="tabpanel" aria-labelledby="">
+    </div>  
   </div>
-  </div>  
   `;
-  
-  $(el).append(tmpl({ 
-    save: config.profile.save,
-    url: config.profile.url,
-    access_check: config.profile.access_check,
-    pic: config.profile.pic,
-    description: config.profile.description,
-    uniq_url: config.profile.uniq_url ,  
+
+  $(el).append(tmpl({
+    profile: config.profile.profile,  
+    statistics: config.profile.statistics,
   }));
 };
 
@@ -194,6 +147,7 @@ const tmpl = () => `
           <thead>
             <tr>
               <th>Место</th>
+              <th>Название сайта</th>
               <th>Кол-во голосов</th>
               <th>Распостранить</th>              
             </tr>
@@ -201,84 +155,16 @@ const tmpl = () => `
           <tbody>
             <tr>
               <td>1</td>
+              <td>Винни-Пух</td>
               <td>100</td>
               <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp" data-url="https://localhost:8080"></div></td>
             </tr>
             <tr>
               <td>2</td>
+              <td>Лось и белки</td>
               <td>99</td>
               <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>98</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>97</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>96</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>95</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>94</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>93</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>9</td>
-              <td>92</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>10</td>
-              <td>91</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>11</td>
-              <td>90</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>12</td>
-              <td>89</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>13</td>
-              <td>88</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>14</td>
-              <td>87</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>15</td>
-              <td>86</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
-            <tr>
-              <td>16</td>
-              <td>85</td>
-              <td><div class="ya-share2" data-curtain data-size="s" data-shape="round" data-services="vkontakte,facebook,odnoklassniki,telegram,twitter,whatsapp"></div></td>              
-            </tr>
+            </tr>           
           </tbody>
         </table>
       </div>
