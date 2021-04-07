3a82a31d3109f5b52ea4ba84574db322f3e2499a
Egor Nagornov <nagornov_e@bk.ru>
2021-04-07 22:08:58 +0700

    Загрузка данных профиля, изменения профиля без валидации формы

--- frontend/src/js/profile.js
+++ frontend/src/js/profile.js
@@ -2,12 +2,14 @@ import config from '../config/config.json'
 import { createAuthWindow, createFooter, createHeader, userLogged } from './templates/main.tmpl';
 import { createProfileTabs, createProfile, createStatistics, createChart } from './templates/profile.tmpl';
 import { createSiteAwards } from './templates/index.tmpl';
-import { checkAuth } from './lib/auth' 
+import { checkAuth, loadProfile, updateProfile } from './lib/auth' 
 import '../styles/style.scss';
+
 const container = () => `
 <div id="profile-main" class="container">
 </div>
 `;
+
 document.title = config.profile.page_title;
 createHeader(document.body);
 createAuthWindow(document.body);
@@ -20,7 +22,24 @@ createSiteAwards('#statistics-tab');
 createChart('#statistics-tab');
 
 checkAuth().done(function(data) {
-  userLogged()
+  userLogged();
+  loadProfile().done(function(data) {
+    const parsed = JSON.parse(data.data);
+    $('#profile-fio').val(parsed.fullname);
+    $('#profile-email').val(parsed.email);
+    $('#profile-number').val(parsed.phone);    
+  });
+  $('#profile-edit-form').on('submit', function(e) {
+    let formFields = $(this).serializeArray();    
+    formFields = formFields.reduce((obj, i) => {      
+      obj[i.name] = i.value;
+      return obj;
+    }, {});
+    updateProfile(formFields);
+    e.preventDefault();
+  });
 }).fail(function(data) {
   console.log('fail', data);
 });
+
+
